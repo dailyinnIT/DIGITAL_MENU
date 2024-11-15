3e84cfb747172b6eee77b68463ba3dc4e64894b1
dailyinnIT <dailyinnhotel.it@gmail.com>
2024-11-15 10:32:55 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -120,5 +120,17 @@ const searchInput = document.getElementById("searchInput");
 
     typeAnimation();
 
-
+    window.onscroll = function () {
+        const scrollButton = document.getElementById("scrollToTop");
+        if (document.documentElement.scrollTop > 200) {
+          scrollButton.style.display = "block";
+        } else {
+          scrollButton.style.display = "none";
+        }
+      };
+      
+      function scrollToTop() {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+      }
+      
     
